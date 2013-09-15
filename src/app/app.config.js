e45f752187d30a3a33ae98d534d852428716eec7
Aladdin Mhaimeed <aladdinmhaimeed@gmail.com>
2013-09-15 04:11:11 +0200

    add auto local/remote url

--- src/app/app.config.js
+++ src/app/app.config.js
@@ -6,7 +6,14 @@ var appConfig = new function()
 	
 	this.defaultState = 'wrapper.dashboard';
 
-	this.yiiUrl = '../yii';
+//Aladdin yii url stuff
+	this.localUrl = '../yii';
+	this.remoteUrl = 'yii';
+	this.getYiiUrl = function(){
+		return document.URL.indexOf('//localhost/')>0?this.localUrl:this.remoteUrl;
+	}
+	this.yiiUrl = this.getYiiUrl();
+///Aladdin
 
 	this.restfulApiBaseUrl = this.yiiUrl;
 }
