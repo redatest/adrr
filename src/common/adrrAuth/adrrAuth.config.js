b70667b0237ede5a5b9974a9d412c755c6f34ee1
Raeef Refai <raeef.refai@gmail.com>
2013-09-15 14:52:37 +0300

    fix
    Signed-off-by: Raeef Refai <raeef.refai@gmail.com>

--- src/common/adrrAuth/adrrAuth.config.js
+++ src/common/adrrAuth/adrrAuth.config.js
@@ -1,7 +1,7 @@
 var adrrAuthConfig =
 {
-	loginUrl: appConfig.yiiUrl+'/api/user/jsonLogin/',
-	logoutUrl: appConfig.yiiUrl+'/user/logout',
+	loginUrl: appConfig.yiiUrl + '/api/user/jsonLogin/',
+	logoutUrl: appConfig.yiiUrl + '/user/logout',
 	loginState: 'login',
 	logoutState: 'logout',
 	defaultState: 'wrapper.dashboard'
