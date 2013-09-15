8ae8c98ac0108d013282a130300cdfeb6307a19e
Raeef Refai <raeef.refai@gmail.com>
2013-09-15 14:48:39 +0300

    add yii
    Signed-off-by: Raeef Refai <raeef.refai@gmail.com>

--- src/app/app.config.js
+++ src/app/app.config.js
@@ -5,13 +5,6 @@ var appConfig = new function()
 	this.defaultRoute = '/dashboard';
 	
 	this.defaultState = 'wrapper.dashboard';
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 6398a81c96a3aacc992ae61769144802b2a0961b
-=======
->>>>>>> 6398a81c96a3aacc992ae61769144802b2a0961b
 	
 	this.localUrl = '../yii';
 	
@@ -23,26 +16,6 @@ var appConfig = new function()
 	}
 	
 	this.yiiUrl = this.getYiiUrl();
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 6398a81c96a3aacc992ae61769144802b2a0961b
-=======
-
-//Aladdin yii url stuff
-	this.localUrl = '../yii';
-	this.remoteUrl = 'yii';
-	this.getYiiUrl = function(){
-		return document.URL.indexOf('//localhost/')>0?this.localUrl:this.remoteUrl;
-	}
-	this.yiiUrl = this.getYiiUrl();
-///Aladdin
->>>>>>> cbb2b8ac391e3f642f9bab8520d5cb8f9eeab910
-<<<<<<< HEAD
->>>>>>> 6398a81c96a3aacc992ae61769144802b2a0961b
-=======
->>>>>>> 6398a81c96a3aacc992ae61769144802b2a0961b
 
 	this.restfulApiBaseUrl = this.yiiUrl;
 }
