637c0717db88295e049a014e596ffd7b0fe5476f
Aladdin Mhaimeed <aladdinmhaimeed@gmail.com>
2013-09-18 16:56:06 +0200

    delete few yii files and split config

---
+++ yii/protected/config/local_mrc.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: علاء الدين المحيميد
+ * Date: 18/09/13
+ * Time: 08:49 ص
+ *
+ */
+
+// don't forget to add this file to .gitignore
+return
+	array(
+		'components'=>
+			array('db'=>
+				array
+				(
+					'connectionString' => 'mysql:host=localhost;dbname=heptc1_conc',
+					'emulatePrepare' => true,
+					'username' => 'root',
+					'password' => '',
+					'charset' => 'utf8',
+					'tablePrefix' => 'tbl_'
+				)
+			),
+			array('db2'=>
+			      array
+			      (
+				      'class'=>'CDbConnection',
+				      'connectionString' => 'mysql:host=localhost;dbname=heptc1_conc',
+				      'emulatePrepare' => true,
+				      'username' => 'root',
+				      'password' => '',
+				      'charset' => 'utf8',
+				      'tablePrefix' => 'tbl_'
+			      )
+			)
+	);
+?>
