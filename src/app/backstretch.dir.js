d4542cc448feecf11ccaa63eddab6eb70b4efa53
Aladdin Mhaimeed <aladdinmhaimeed@gmail.com>
2013-10-19 19:00:24 +0200

    merge

--- src/app/backstretch.dir.js
+++ src/app/backstretch.dir.js
@@ -4,7 +4,6 @@ adrrApp.directive(
 	    return {
 		    restrict: 'A',
 		    link: function (scope, element, attr) {
-			    console.log('eeeeeeee');
 			    element.backstretch([attr.bg1,attr.bg2,attr.bg3], {duration: 3000, fade: 750});
 		    }
 	    };
