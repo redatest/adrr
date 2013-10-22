214963d6100086682bf0805d8fd6f6b7099c4944
Aladdin Mhaimeed <aladdinmhaimeed@gmail.com>
2013-10-22 19:41:59 +0200

    add little comments to getData function

--- src/common/services/adrrDataGetter/adrrDataGetter.js
+++ src/common/services/adrrDataGetter/adrrDataGetter.js
@@ -1,70 +1,74 @@
-angular.module('adrrDataGetter', [], null).factory
+var adrrDataGetter = angular.module('adrrDataGetter', [], null).factory
 (
-    'adrrDataGetter', function ($http) {
+	'adrrDataGetter', function ($http) {
 
-        var targets = [];
+		var targets = [];
 
-        var timers = [];
+		var timers = [];
 
-        var getData = function (sourceUrl, target, method, args) {
+		//getData is the function that actually fetches data. It is used by function set.
+		//  sourceUrl: the url to the requested data source
+		//  target: is an array object. It has to be initialized as an empty array before calling getData.
+		//  method: post or get.
+		//  args: any additional paramitures to be passed with the request.
+		var getData = function (sourceUrl, target, method, args) {
 
-            $http
-            ({
-                method: (method !== undefined ? method : 'GET'),
-                url: sourceUrl,
-                data: (args !== undefined ? args : null)
-            }).success
-            (
-                function (data) {
-                    var targetIndex = _.indexOf(targets, target, 0);
+			$http
+			({
+				 method: (method !== undefined ? method : 'GET'),
+				 url: sourceUrl,
+				 data: (args !== undefined ? args : null)
+			 }).success
+			(
+				function (data) {
+					var targetIndex = _.indexOf(targets, target, 0);
 
-                    if (angular.isArray(data)) {
-                        var dataLength = data.length;
+					if (angular.isArray(data)) {
+						var dataLength = data.length;
 
-                        for (var i = 0; i < dataLength; i++) {
+						for (var i = 0; i < dataLength; i++) {
 
-                            if (_.indexOf(targets[targetIndex], data[i], 0) !== -1) {
-                                targets[targetIndex].push(data[i]);
-                            }
-                        }
+							if (_.indexOf(targets[targetIndex], data[i], 0) !== -1) {
+								targets[targetIndex].push(data[i]);
+							}
+						}
 
-                        dataLength = targets[targetIndex].length;
+						dataLength = targets[targetIndex].length;
 
-                        for (i = 0; i < dataLength; i++) {
+						for (i = 0; i < dataLength; i++) {
 
-                            if (_.indexOf(data, targets[targetIndex][i], 0) !== -1) {
-                                targets[targetIndex].splice(i, 1);
-                            }
-                        }
-                    }
-                }
-            )
-        };
+							if (_.indexOf(data, targets[targetIndex][i], 0) !== -1) {
+								targets[targetIndex].splice(i, 1);
+							}
+						}
+					}
+				}
+			)
+		};
 
-        var set = function (sourceUrl, target, time, method, args) {
+		var set = function (sourceUrl, target, time, method, args) {
 
-            if (time !== undefined) {
-                var timer = window.setInterval(getData.bind(sourceUrl, target, method, args), time);
+			if (time !== undefined) {
+				var timer = window.setInterval(getData.bind(sourceUrl, target, method, args), time);
 
-                timers.push(timer);
+				timers.push(timer);
 
-                targets.push(target);
-            }
-        };
+				targets.push(target);
+			}
+		};
 
-        var unset = function (target) {
+		var unset = function (target) {
 
-            var targetIndex = _.indexOf(targets, target, 0);
+			var targetIndex = _.indexOf(targets, target, 0);
 
-            window.clearInterval(timers[targetIndex]);
+			window.clearInterval(timers[targetIndex]);
 
-            targets.splice(targetIndex, 1);
-        };
+			targets.splice(targetIndex, 1);
+		};
 
-        return {
-
-            set: set,
-            unset: unset
-        }
-    }
+		return {
+			set: set,
+			unset: unset
+		}
+	}
 );
