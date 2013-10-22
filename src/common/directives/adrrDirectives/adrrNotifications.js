6b913a4ea197589c0f7349d3138750f1fa2ecd53
Raeef Refai <raeef.refai@gmail.com>
2013-10-22 20:02:28 +0300

    add notifications directive

---
+++ src/common/directives/adrrDirectives/adrrNotifications.js
@@ -0,0 +1,19 @@
+angular.module('adrrDirectives.notifications', [], null)
+
+    .directive
+(
+    'adrrNotifications', function () {
+
+        return {
+
+            restrict: 'E',
+
+            scope: {},
+
+            link: function (scope, element, attrs) {
+
+
+            }
+        }
+    }
+);
