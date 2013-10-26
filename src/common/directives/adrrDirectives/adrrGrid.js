4776af5eaf419f8e6ef61c7aa19e8c5269fd55e9
Raeef Refai <raeef.refai@gmail.com>
2013-10-27 02:22:55 +0300

    Add adrrGrid.js

---
+++ src/common/directives/adrrDirectives/adrrGrid.js
@@ -0,0 +1,16 @@
+var adrrGrid = angular.module('adrrDirectives.grid', [], null)
+
+    .directive
+(
+    'adrrGrid', function () {
+        return {
+            restrict: 'E',
+
+            scope: true,
+
+            link: function (scope, element, attrs) {
+
+            }
+        }
+    }
+);
