b83330eb3427f78e1decb5ad6103a822944712a4
reda test <redatest7@gmail.com>
2013-11-28 15:08:15 +0100

    fixing autocompletion on plant field -working on real data

--- src/app/wrapper/eng/autocomp.js
+++ src/app/wrapper/eng/autocomp.js
@@ -1,15 +1,36 @@
 angular.module('adrrApp.wrapper.autocomp', [], null).
-    controller('myCtrl', function($scope){
-        $scope.testname = 'med_reda';
 
-        $scope.planlist = [
-            { 'name' : 'Blaze Commando' },
-            { 'name' : 'Uncovered Clues' },
-            { 'name' :  '111'},
-            { 'name' :  '11'}
+    
+    controller('myCtrl', function($scope, Restangular){
+        
+        
+        $scope.planlist = [];
+        var li = [];
+
+        // getting list of available Plants
+        Restangular.all('eng/labPlant').getList().then(function(data){
+
+            for (var i = data.length - 1; i >= 0; i--) {
+                var elem = {};
+                elem.plant = data[i]['plant'];
+                li.push(elem);
+            };
+            $scope.planlist = li;
+
+        });
+
+        // for testing : to be deleted later
+        $scope.planlist2 = [
+            { 'plant' : 'Blaze Commando' },
+            { 'plant' : 'Uncovered Clues' },
+            { 'plant' :  '1112323'},
+            { 'plant' :  '11'}
         ];
 
         $scope.plan = null;
+        
+        
+
     }).
     directive('planAutocomplete', ['$http', function($http) {
     return {
@@ -19,7 +40,7 @@ angular.module('adrrApp.wrapper.autocomp', [], null).
             function filterPlan(array, term) {
                 var matcher = new RegExp('(' + $.ui.autocomplete.escapeRegex(term) + ')', 'gi');
                 return $.grep(array, function (item) {
-                    return matcher.test(item.name);
+                    return matcher.test(item.plant);
                 });
             }
 
@@ -31,10 +52,8 @@ angular.module('adrrApp.wrapper.autocomp', [], null).
             // elem is a jquery lite object if jquery is not present, but with jquery and jquery ui, it will be a full jquery object.
             elem.autocomplete({
                 source: function(request, response) {
-                    // exemple on rest field
-                    /*$http.get("/magicsupremacy/rest/api/1/card/search?name=" + this.term).success(function(data) {
-                     response(data.cardslist);
-                     });*/
+
+                    // console.log(Restangular.all('eng/labPlant').getList());
 
                     response(filterPlan(scope.planlist, this.term));
                 },
@@ -44,7 +63,7 @@ angular.module('adrrApp.wrapper.autocomp', [], null).
                 },
                 select: function(event, ui) {
 
-                    scope.plan = ui.item.label;
+                    scope.plan = parseInt(ui.item.label);
                     scope.$apply();
                     return false;
                 },
@@ -52,9 +71,9 @@ angular.module('adrrApp.wrapper.autocomp', [], null).
 
             }).data("ui-autocomplete")._renderItem = function(ul, item) {
 
-                item.label = item.name;
+                item.label = item.plant;
 
-                var planNameHighlighted = highlightPlan(item.name, this.term);
+                var planNameHighlighted = highlightPlan(item.plant, this.term);
 
 
                 var planLine = $("<div>").html(planNameHighlighted);
